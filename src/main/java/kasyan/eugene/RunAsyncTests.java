9037b34aea5e644468d9878dcc383d2c81d9b0db
JKasyan <j.kasyan86@gmail.com>
2018-02-07 02:08:54 +0200

    Initial commit

---
+++ src/main/java/kasyan/eugene/RunAsyncTests.java
@@ -0,0 +1,102 @@
+package kasyan.eugene;
+
+import com.sforce.soap.tooling.QueryResult;
+import com.sforce.soap.tooling.TestLevel;
+import com.sforce.soap.tooling.ToolingConnection;
+import com.sforce.soap.tooling.sobject.AsyncApexJob;
+import com.sforce.soap.tooling.sobject.SObject;
+import com.sforce.ws.ConnectionException;
+import com.sforce.ws.ConnectorConfig;
+import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.Task;
+
+public class RunAsyncTests extends Task {
+
+    private String username;
+    private String password;
+    private String URL;
+    private ToolingConnection connection;
+    private static final String JOB_COMPLETED_STATUS = "Completed";
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getURL() {
+        return URL;
+    }
+
+    public void setURL(String URL) {
+        this.URL = URL;
+    }
+
+    @Override
+    public void execute() throws BuildException {
+        authenticate();
+        String asyncApexJobId = runAsyncTestAndGetJobId();
+        waitWhileAsyncJobNotCompleted(asyncApexJobId);
+
+    }
+
+    private void authenticate() {
+        String endpoint = URL + "services/Soap/T/42.0";
+
+        ConnectorConfig connectorConfig = new ConnectorConfig();
+        connectorConfig.setUsername(username);
+        connectorConfig.setAuthEndpoint(endpoint);
+        connectorConfig.setPassword(password);
+
+        try {
+            connection = new ToolingConnection(connectorConfig);
+        } catch (ConnectionException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private String runAsyncTestAndGetJobId() {
+        String asyncApexJobId;
+        try {
+            asyncApexJobId = connection.runTestsAsynchronous(null, null, 0,
+                    TestLevel.RunAllTestsInOrg,
+                    null,
+                    null,
+                    null);
+        } catch (ConnectionException e) {
+            throw new RuntimeException(e);
+        }
+        return asyncApexJobId;
+    }
+
+    private String loadAsyncApexJobAndGetStatus(String asyncApexJobId) throws ConnectionException {
+        QueryResult query = connection.query("SELECT Status FROM AsyncApexJob WHERE Id='"
+                + asyncApexJobId + "'");
+        SObject[] asyncApexJobs = query.getRecords();
+        AsyncApexJob asyncApexJob = (AsyncApexJob) asyncApexJobs[0];
+        log("Status === > " + asyncApexJob.getStatus());
+        return asyncApexJob.getStatus();
+    }
+
+    private void waitWhileAsyncJobNotCompleted(String asyncApexJobId) {
+        try {
+            String asyncApexJobStatus = null;
+            while (!JOB_COMPLETED_STATUS.equals(asyncApexJobStatus)) {
+                asyncApexJobStatus = loadAsyncApexJobAndGetStatus(asyncApexJobId);
+                Thread.sleep(1000);
+            }
+        } catch (ConnectionException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
